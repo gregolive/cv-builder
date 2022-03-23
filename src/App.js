e6b42e333f3c85b3b925ec2dcbaa6c5aa65936a5
Gregory Olive <gregoryolive@outlook.com>
2022-03-23 16:08:15 +0900

    Complete footer and add personalinfo files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
     <div>
-      
+      <main>
+        
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+         <p>&copy; <a href='https://github.com/gregolive' target='_blank' rel='noreferrer'>gregolive</a></p>
+      </footer>
+    )
+  };
+};
+
+export default Footer;
